824aee7b84a8cbc5978befb8fcd629ec278ca01c
Avirup0321 <55937969+Avirup0321@users.noreply.github.com>
2021-07-31 22:29:19 +0530

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,92 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
-      ),
-    ),
-  );
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key key}) : super(key: key); // constructor
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          backgroundColor: Colors.teal,
+          body: SafeArea(
+            child:Column(
+              mainAxisAlignment:MainAxisAlignment.center ,
+                children: <Widget>[
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage("Image/Avirup.jpg"),
+              ),
+              Text(
+                "Avirup Chattopadhyay",
+                style: TextStyle(
+                    fontFamily: 'Dancing Script',
+                    fontSize: 40,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold),
+              ),
+              Text(
+                "KIIt Student",
+                style: TextStyle(
+                  fontFamily: 'Quicksand',
+                  fontSize: 20,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 20.0,width: 150.0,
+              child: Divider(color: Colors.white,)),
+              Card(
+                color: Colors.white,
+
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
+                child: Padding(
+                  padding: const EdgeInsets.all(4.0),
+                  child: Row(children: [
+                    Icon(
+                      Icons.phone_android,
+                      color: Colors.teal,
+                    ),
+                    SizedBox(
+                      width: 50.0,
+                    ),
+                    Text(
+                      "9635333004",
+                      style: TextStyle(
+                        fontFamily: 'Quicksand',
+                        fontSize: 20,
+                        color: Colors.teal,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    )
+                  ]),
+                ),
+              ),
+              Card(
+
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
+                color: Colors.white,
+                child: ListTile(leading:Icon(
+                  Icons.email,
+                  color: Colors.teal,
+                ),
+                  title:Text(
+                    "avirupchatterjee520@yahoo.com",
+                    style: TextStyle(
+                      fontFamily: "Quicksand",
+                      fontSize: 20,
+                      color: Colors.teal,
+                        fontWeight: FontWeight.bold
+                    ),
+                  ) ,
+                ),
+              )
+            ]),
+            )),
+        );
+  }
 }
